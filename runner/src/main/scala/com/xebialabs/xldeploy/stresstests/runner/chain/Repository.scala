4bfafbb0bd850faf90ec6bb9f6850002f271b682
Ege Aksoz <eaksoz@xebialabs.com>
2018-12-03 12:58:46 +0100

    DEPL-13597: Adds customer simulation

--- runner/src/main/scala/com/xebialabs/xldeploy/stresstests/runner/chain/Repository.scala
+++ runner/src/main/scala/com/xebialabs/xldeploy/stresstests/runner/chain/Repository.scala
@@ -2,6 +2,7 @@ package com.xebialabs.xldeploy.stresstests.runner.chain
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.http.request.StringBody
 
 object Repository {
 
@@ -11,5 +12,10 @@ object Repository {
 
   def delete(id: String) =
     exec(http("delete CI").delete(s"/repository/ci/$id"))
+
+  def readCIS() =
+    exec(http("Read Multiple Configuration").post("/repository/cis/read").
+      body(StringBody(s"""{[]}""")).asJSON.
+      check(status.is(200)))
 
 }
